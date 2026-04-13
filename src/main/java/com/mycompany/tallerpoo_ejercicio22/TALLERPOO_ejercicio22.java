551c57f2509f28cf373b8abfd48bbd831ae423fc
samuelmurilloa <samuel.murillo_a@uao.edu.co>
2026-04-13 07:45:06 -0500

    primer commit, iniciar repositorio

---
+++ src/main/java/com/mycompany/tallerpoo_ejercicio22/TALLERPOO_ejercicio22.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerpoo_ejercicio22;
+
+/**
+ *
+ * @author samue
+ */
+public class TALLERPOO_ejercicio22 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
