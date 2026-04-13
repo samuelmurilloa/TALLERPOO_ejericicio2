971d68ace88f5385dbceadcbb2eea1799bc2b2f9
samuelmurilloa <samuel.murillo_a@uao.edu.co>
2026-04-13 07:53:41 -0500

    segundo commit, crear punto modelo con atributos, constrcutor y metodos basicos

---
+++ src/main/java/Modelo/PuntoModelo.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author samue
+ */
+
+public class PuntoModelo {
+    
+    private int x;
+    private int y;
+
+    public PuntoModelo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
